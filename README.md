87d13b90ceb9b4deb5dcba6e611aa6f482144cf0
Selin ATMACA <selinatmacayy@gmail.com>
2025-02-09 11:12:42 +0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
+# MVC : Model - View
+
+- Model: Veri yapısını tuttuğumuz dosyalar
+
+- View: Kullanıcının gördüğü arayüz bileşenleri
+
+- Controllers: Kullanıcı etkileşime göre neler yapılacağını söyleriz
+
+# Kullanacağımız Mimari
+
+- routes: gelen isteklere göre çalışacak ve cevap verilecek olan fonksiyonları tetikler
+
+- controllers: cevap gönderen fonksşyonlar
+
+- middleware: arayazılımlar
+
+- utils: yardımcı fonksiyonlar
+
+- model: read ve write fonksiyonları
+
 # Fullstack_RecipeApp
--- api/routes/recipeRoutes.js
+++ api/routes/recipeRoutes.js
@@ -8,8 +8,10 @@ import {
 } from "../controllers/recipeController.js";
 import controlId from "../middleware/controlId.js";
 
+// Router > server.js dosyası dışarısında route tanımı yapmamıza olanak sağlar.
 const router = express.Router();
 
+//oluşturduğumuz routerın endpoint/route/yol larını  ve istek gelince çalışacak fonksiyonları belirle
 router.route("/api/v1/recipes").get(getAllRecipes).post(createRecipes);
 
 router
@@ -18,4 +20,5 @@ router
   .delete(controlId, deleteRecipe)
   .patch(controlId, updateRecipe);
 
+//serverda kullanmak için routerı export et
 export default router;
